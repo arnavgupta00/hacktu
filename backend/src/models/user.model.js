904e8e9f6f978f8c8390783635b094aeab4c64c5
Jashany <jsingh12_be22@thapar.edu>
2024-02-09 19:10:05 +0530

    hehe

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    branch: {
+
+    },
+    contact :{
+
+    },
+    post 
+
+});
